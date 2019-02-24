bbcf98d32d2ffaeeb82f0ac4eec37a167a56461c
Devin Bombay <dbombay@bdtrust.org>
2019-02-24 17:14:55 -0500

    Transfers FE files from symfony dir and gets them running again;

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {CategoriesCollection, CreateObjectModal, Hero} from "../components"
+
+export default class HomeContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <Hero />
+        <CategoriesCollection />
+      </div>
+    )
+  }
+}
